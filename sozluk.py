fa63c37a687c6e5ad468988ed990d0ed4e61e460
elifefsakbas <157141486+elifefsakbas@users.noreply.github.com>
2024-01-20 13:32:00 +0300

    Create sozluk.py

---
+++ sozluk.py
@@ -0,0 +1,18 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL": "bir şakayı karşılık cevap",
+            "SHEESH":"onaylamak",
+            "CREEPY":"korkunç",
+            "AGGRO":"agresifleşmek/sinirlenmek"
+            }
+            
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+
+
+if word in meme_dict.keys():
+   print(meme_dict[word])
+else:
+    print("bu kelimeyi bilmiyorum... tekrar dene!!")
